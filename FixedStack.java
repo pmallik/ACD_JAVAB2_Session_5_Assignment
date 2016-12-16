e3a0ce6829f7698f76cea9d1a57933aee49ea8ba
pmallik <puja_mallik@yahoo.com>
2016-12-16 13:04:18 +0530

    Add files via upload
    This is still work in progress so please don't mark it as solution

---
+++ FixedStack.java
@@ -0,0 +1,31 @@
+package myPack;
+
+public class FixedStack implements Stack {
+	int size;
+	FixedStack(int i)
+	{
+		System.out.println("Size of the stack is " + i);
+		size=i;
+	}
+	
+	public void push(int i)
+	{
+		if (i > size)
+		{
+			System.out.println("Stack is full");
+		}
+	}
+	
+	public int pop()
+	{
+		return 1;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         FixedStack fs = new FixedStack(10);
+         
+         fs.push(20);
+	}
+
+}
---
+++ Stack.java
@@ -0,0 +1,8 @@
+package myPack;
+
+public interface Stack {
+	void push(int i);
+	int pop();
+
+
+}
